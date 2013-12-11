4314be8bedd12edb418ce66215fe3306199de975
Max Lynch <max@drifty.com>
2013-12-11 09:25:30 -0600

    Updated API key

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 angular.module('ionic.weather', ['ionic', 'ionic.weather.services', 'ionic.weather.filters', 'ionic.weather.directives'])
 
-.constant('WUNDERGROUND_API_KEY', '1cc2d3de40fa5af0')
+.constant('WUNDERGROUND_API_KEY', 'ae228fa54c49352b')
 
 .constant('FLICKR_API_KEY', '504fd7414f6275eb5b657ddbfba80a2c')
 
